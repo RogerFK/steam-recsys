f095b00038208298ba8bac39b1b6a004714a562a
Jorge González Gómez <jorge.gonzalezgomez01@estudiante.uam.es>
2023-03-13 13:14:07 +0100

    Finished the important parts of normalization
    Signed-off-by: Jorge González Gómez <jorge.gonzalezgomez01@estudiante.uam.es>

--- normalization.py
+++ normalization.py
@@ -2,14 +2,21 @@ from abc import ABCMeta, abstractmethod
 import pandas as pd
 from pandas.core.api import DataFrame
 import numpy as np
+import math
+import pandas.core.groupby as pdg
+# import GroupBy
+from pandas.core.groupby.groupby import GroupBy
 
 class PlaytimeNormalizerBase(metaclass=ABCMeta):
-    def __init__(self, inplace: bool = False):
+    def __init__(self, denominator_function: str, playtime_approach: str = "minutes_always_more_than_60", inplace: bool = False):
         self.inplace = inplace
-    @abstractmethod
-    def _normalize_function(self, playtime, max_playtime):
-        return 60 if playtime < 60 else playtime
+        self.denominator_function = denominator_function
+        self.playtime_approach = playtime_approach
 
+    @abstractmethod
+    def normalize_value(self, playtime, denominator):
+        pass
+    
     def normalize(self, data: DataFrame) -> DataFrame:
         """The data to normalize, with a structure of:
             "steamid","appid","playtime_forever"
@@ -27,11 +34,31 @@ class PlaytimeNormalizerBase(metaclass=ABCMeta):
         # Normalize each player with its own max playtime (for example, if a player has 50.000h in HoI4, and 10h in Civ5, the HoI4 playtime will be normalized to 1, and the Civ5 playtime to 0.2)
         # This is done to prevent players with a lot of playtime in a single game from dominating the recommendations
         # Also, any playtime below 60 minutes counts as 60 minutes, playtime below 1 hour means the user has shown interest in the game, but isn't very important
-    
-        # data["playtime_forever"] = data["playtime_forever"].apply(lambda x: 60 if x < 60 else x)
-        max_playtimes = data.groupby("steamid")["playtime_forever"].max()
+
+        # we need this to prevent division by zero, wrong sum, max, etc.
+        if self.playtime_approach == "ignore":
+            pass
+        elif self.playtime_approach == "minutes_always_more_than_60":
+            data["playtime_forever"] = data["playtime_forever"].apply(lambda x: 60 if x < 60 else x)
+        elif self.playtime_approach == "hours_but_add_1":
+            data["playtime_forever"] = data["playtime_forever"].apply(lambda x: math.floor(x / 60) + 1)
+        else:
+            raise ValueError("playtime_approach must be 'minutes_always_more_than_60', 'hours_but_add_1' or 'ignore'")
+
+        # NOTE: I benchmarked this and having no if statement is a tiny bit faster, but there's no noticeable difference
+        # also, normalization is usually not the bottleneck, so it doesn't matter
+        if self.denominator_function == "max":
+            denominators = data.groupby("steamid")["playtime_forever"].max()
+        elif self.denominator_function == "mean":
+            denominators = data.groupby("steamid")["playtime_forever"].mean()
+        elif self.denominator_function == "sum":
+            denominators = data.groupby("steamid")["playtime_forever"].sum()
+        elif self.denominator_function == "median":
+            denominators = data.groupby("steamid")["playtime_forever"].median()
+        else:
+            raise ValueError("divide_by must be either 'max', 'mean', 'sum' or 'median'")
         
-        data["playtime_forever"] = data.apply(lambda x: self._normalize_function(x["playtime_forever"], max_playtimes[x["steamid"]]), axis=1)
+        data["playtime_forever"] = data.apply(lambda x: self.normalize_value(x["playtime_forever"], denominators[x["steamid"]]), axis=1)
         return data
     
     def validate_data(self, data: DataFrame):
@@ -51,24 +78,21 @@ class PlaytimeNormalizerBase(metaclass=ABCMeta):
 class LinearPlaytimeNormalizer(PlaytimeNormalizerBase):
     """Normalizes the player_games data using the playtime_forever
     """
-    def _normalize_function(self, playtime, max_playtime):
-        playtime = super()._normalize_function(playtime, max_playtime)
-        return playtime / max_playtime
+    def normalize_value(self, playtime, denominator):
+        return playtime / denominator
 
 class LogPlaytimeNormalizer(PlaytimeNormalizerBase):
     """Normalizes the player_games data using the log of the playtime_forever
     """
-    def _normalize_function(self, playtime, max_playtime):
-        playtime = super()._normalize_function(playtime, max_playtime)
-        return np.log(playtime) / np.log(max_playtime)
+    def normalize_value(self, playtime, denominator):
+        return np.log(playtime) / np.log(denominator)
 
 class RootPlaytimeNormalizer(PlaytimeNormalizerBase):
     """Normalizes the player_games data using the N root of the playtime_forever
     """
-    def __init__(self, nroot: int = 2, inplace: bool = False):
-        super().__init__(inplace=inplace)
+    def __init__(self, divide_by: str, nroot: int = 2, inplace: bool = False):
+        super().__init__(denominator_function=divide_by, inplace=inplace)
         self.nroot = nroot
 
-    def _normalize_function(self, playtime, max_playtime):
-        playtime = super()._normalize_function(playtime, max_playtime)
-        return np.power(playtime, 1/self.nroot) / np.power(max_playtime, 1/self.nroot)
+    def normalize_value(self, playtime, denominator):
+        return np.power(playtime, 1/self.nroot) / np.power(denominator, 1/self.nroot)
--- normalization_tests.py
+++ normalization_tests.py
@@ -1,33 +1,81 @@
 import normalization
 import pandas as pd
+from pandas.testing import assert_frame_equal
+import timeit
+import os
+
 data = pd.DataFrame({
         "steamid":          [   1,   1,    1,    1,     2,  2,   2,   2],
         "appid":            [   1,   2,    3,    4,     1,  2,   3,   4],
         "playtime_forever": [6000, 120, 3000, 3000, 10000, 20, 120, 240]
     })
-norm = normalization.LinearPlaytimeNormalizer()
+# note 20 = 60
+linear_norm_max = normalization.LinearPlaytimeNormalizer('max')
+log_norm_max = normalization.LogPlaytimeNormalizer('max')
+root_norm_max = normalization.RootPlaytimeNormalizer('max')
+
+linear_norm_sum = normalization.LinearPlaytimeNormalizer('sum')
+log_norm_sum = normalization.LogPlaytimeNormalizer('sum')
+root_norm_sum = normalization.RootPlaytimeNormalizer('sum')
+
 def test_normalize():
-    data = pd.DataFrame({
-        "steamid":          [1,      1,    1,    1,     2,  2,   2,   2],
-        "appid":            [1, 2, 3, 4, 1, 2, 3, 4],
-        "playtime_forever": [6000, 120, 3000, 3000, 10000, 20, 120, 240]
-    })
-    # note 20 = 60
-    expected = pd.DataFrame({
-        "steamid": [1, 1, 1, 1, 2, 2, 2, 2],
-        "appid": [1, 2, 3, 4, 1, 2, 3, 4],
-        "playtime_forever": [1, 0.02, 0.5, 0.5, 1, 0.006, 0.012, 0.024]
-    })
+    if not os.path.exists("./test_output"):
+        os.mkdir("./test_output")
     print("Input\n", data)
-    normalized = norm.normalize(data)
-    print("Input again\n", data)
-    print("Output\n", normalized)
-    print("Expected\n", expected)
+
+    print ("----------------\nUsing max normalization\n----------------")
+
+    linear_expected_max = pd.read_csv("./test_expected/linear_normalized_max.csv")
+    print("Linear output\n", linear_norm_max.normalize(data))
+    normalized = linear_norm_max.normalize(data)
+    normalized.to_csv("./test_output/linear_normalized_max.csv", index=False)
+    assert_frame_equal(normalized, linear_expected_max)
+    print("Timing for linear normalization (max)")
+    print(timeit.timeit("linear_norm_max.normalize(data)", setup="from normalization_tests import linear_norm_max, data", number=1000))
+
+    log_expected_max = pd.read_csv("./test_expected/log_normalized_max.csv")
+    normalized = log_norm_max.normalize(data)
+    print("Log output\n", normalized)
+    normalized.to_csv("./test_output/log_normalized_max.csv", index=False)
+    print(log_expected_max)
+    assert_frame_equal(normalized, log_expected_max)
+    print("Timing for log normalization (max)")
+    print(timeit.timeit("log_norm_max.normalize(data)", setup="from normalization_tests import log_norm_max, data", number=1000))
+
+    root_expected_max = pd.read_csv("./test_expected/root_normalized_max.csv")
+    normalized = root_norm_max.normalize(data)
+    print("Root output\n", normalized)
+    normalized.to_csv("./test_output/root_normalized_max.csv", index=False)
+    assert_frame_equal(normalized, root_expected_max)
+    print("Timing for root normalization (max)")
+    print(timeit.timeit("root_norm_max.normalize(data)", setup="from normalization_tests import root_norm_max, data", number=1000))
     
-    assert normalized.equals(expected)
-    import timeit
-    print("Timing")
-    print(timeit.timeit("norm.normalize(data)", setup="from normalization_tests import norm, data", number=1000))
+    print ("----------------\nUsing sum normalization\n----------------")
+    
+    linear_expected_sum = pd.read_csv("./test_expected/linear_normalized_sum.csv")
+    normalized = linear_norm_sum.normalize(data)
+    print("Linear output\n", normalized)
+    normalized.to_csv("./test_output/linear_normalized_sum.csv", index=False)
+    assert_frame_equal(normalized, linear_expected_sum)
+    print("Timing for linear normalization (sum)")
+    print(timeit.timeit("linear_norm_sum.normalize(data)", setup="from normalization_tests import linear_norm_sum, data", number=1000))
+
+    log_expected_sum = pd.read_csv("./test_expected/log_normalized_sum.csv")
+    normalized = log_norm_sum.normalize(data)
+    print("Log output\n", normalized)
+    normalized.to_csv("./test_output/log_normalized_sum.csv", index=False)
+    assert_frame_equal(normalized, log_expected_sum)
+    print("Timing for log normalization (sum)")
+    print(timeit.timeit("log_norm_sum.normalize(data)", setup="from normalization_tests import log_norm_sum, data", number=1000))
+
+    root_expected_sum = pd.read_csv("./test_expected/root_normalized_sum.csv")
+    normalized = root_norm_sum.normalize(data)
+    print("Root output\n", normalized)
+    normalized.to_csv("./test_output/root_normalized_sum.csv", index=False)
+    assert_frame_equal(normalized, root_expected_sum)
+    print("Timing for root normalization (sum)")
+    print(timeit.timeit("root_norm_sum.normalize(data)", setup="from normalization_tests import root_norm_sum, data", number=1000))          
 
 if __name__ == "__main__":
     test_normalize()
+    print("All tests passed")
